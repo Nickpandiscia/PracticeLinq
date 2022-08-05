dd2e4ae0c605f9d3fe8c77fbed9317abbf64dd7c
Nickpandiscia <npandiscia1@gmail.com>
2022-08-05 13:57:17 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gameList = new List<string>() {"Halo", "Call of Duty", "Forza", "Rust"};
+
+            var newOrder = gameList.OrderByDescending(x => x.Length);
+
+            Console.WriteLine("Here are the games in order by length:");
+            foreach(var game in newOrder)
+            {
+                Console.WriteLine(game);
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
